28ea313ff63030ad2754a10803ee9af66760cee1
Maryna-Dr <drapata.mb@gmail.com>
2022-09-27 22:30:27 +0300

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,28 +3,30 @@ import { galleryItems } from './gallery-items';
 
 // Change code below this line
 // Описаний в документації
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-console.log(SimpleLightbox);
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
 
-const markupGalleryItem = galleryItems.map(it =>
-    `<a class="gallery__link" href="${it.original}">
+const markupGalleryItem = galleryItems
+  .map(
+    it =>
+      `<a class="gallery__link" href="${it.original}">
         <img
         class="gallery__image"
         src="${it.preview}"
         alt="${it.description}"
         />
-    </a>`).join('');
+    </a>`
+  )
+  .join('');
 
 gallery.insertAdjacentHTML('afterbegin', markupGalleryItem);
 
 const titleImage = {
-    captionsData: 'alt',
-    captionDelay: 250,
+  captionsData: 'alt',
+  captionDelay: 250,
 };
 
 new SimpleLightbox('.gallery__link', titleImage);
